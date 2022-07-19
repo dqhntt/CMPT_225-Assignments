652e3267effcbee624a89534a169326f6db3d1d7
dqhntt <48117687+dqhntt@users.noreply.github.com>
2022-07-18 22:58:01 -0700

    Prep for auto tests
    https://stackoverflow.com/a/4331509

--- Assignment 4/a4main.cpp
+++ Assignment 4/a4main.cpp
@@ -1,8 +1,29 @@
 // a4main.cpp
 #include "AVLTree.h"
 #include <iostream>
+#include <unordered_set>
+#include <utility>
 
 namespace {
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                               //
+//      Functions taking trees instead of nodes:                                                 //
+//      - getRoot(tree)                                                                          //
+//      - print(tree)  // Nodes are printed as (Key, Value | Height), left on top, right below.  //
+//      - isValidAVL(tree)                                                                       //
+//                                                                                               //
+//      Functions that can operate on substree rooted at `node` parameter:                       //
+//      Assuming `root` was obtained using getRoot(tree):                                        //
+//      - realSize(root)                                                                         //
+//      - realHeight(root)                                                                       //
+//      - hasChildrenLinkedToParents(root)  // For testing copy ctor.                            //
+//      - hasNoDuplicateKeys(root)                                                               //
+//      - isBalancedAt(root)  // Pre: node->height == realHeight(node).                          //
+//      - isBST(root)                                                                            //
+//      - isAVL(root)  // Less comprehensive than isValidAVL(tree).                              //
+//                                                                                               //
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 // A root with its type.
 template <class Key, class Value, class Node = AVLTreeNode<Key, Value>>
@@ -10,18 +31,17 @@ const Node* getRoot(const AVLTree<Key, Value>& tree) {
     return static_cast<Node*>(tree.getRoot());
 }
 
-// Real size.
 template <class Node>
-int size(const Node* node) {
-    return (node == nullptr) ? 0 : size(node->left) + size(node->right) + 1;
+int realSize(const Node* node) {
+    return (node == nullptr) ? 0 : realSize(node->left) + realSize(node->right) + 1;
 }
 
-// Real height.
 template <class Node>
-int height(const Node* node) {
-    return (node == nullptr) ? -1 : std::max(height(node->left), height(node->right)) + 1;
+int realHeight(const Node* node) {
+    return (node == nullptr) ? -1 : std::max(realHeight(node->left), realHeight(node->right)) + 1;
 }
 
+// Don't call this directly.
 // Cite: https://stackoverflow.com/questions/36802354/print-binary-tree-in-a-pretty-way-using-c
 template <class Node>
 void print(const std::string& prefix, const Node* node, bool isLeft) {
@@ -30,7 +50,7 @@ void print(const std::string& prefix, const Node* node, bool isLeft) {
 
         // print the value of the node
         std::cout << "(" << node->key << ", " << node->value << " | " << node->height << ")\n";
-        assert(height(node) == static_cast<int>(node->height));
+        assert(realHeight(node) == static_cast<int>(node->height));
 
         // enter the next tree level - left and right branch
         print(prefix + (isLeft ? "│   " : "    "), node->left, true);
@@ -40,6 +60,7 @@ void print(const std::string& prefix, const Node* node, bool isLeft) {
     }
 }
 
+// (Key, Value | Height)
 template <class Tree>
 void print(const Tree& tree) {
     std::cout << ".------------------.\n"
@@ -47,16 +68,58 @@ void print(const Tree& tree) {
                  "|----Down-Right----|\n"
                  "*------------------*\n";
     print("", getRoot(tree), false);
-    std::cout << std::endl;
-    assert(size(getRoot(tree)) == static_cast<int>(tree.size()));
+    std::cout << "^Size: " << tree.size() << "^\n" << std::endl;
+    assert(realSize(getRoot(tree)) == static_cast<int>(tree.size()));
 }
 
 template <class Node>
-bool hasChildrenLinkedToParents(const Node* child, const Node* parent = nullptr) {
-    return (child == nullptr)
-        || (child->parent == parent
-            && hasChildrenLinkedToParents(child->left, child)
-            && hasChildrenLinkedToParents(child->right, child));
+bool hasChildrenLinkedToParents(const Node* node, const Node* parent = nullptr) {
+    return (node == nullptr)
+        || (node->parent == parent
+            && hasChildrenLinkedToParents(node->left, node)
+            && hasChildrenLinkedToParents(node->right, node));
+}
+
+template <class Key, class Value, class HashSet = std::unordered_set<Key>>
+bool hasNoDuplicateKeys(const AVLTreeNode<Key, Value>* node, HashSet&& set = {}) {
+    return (node == nullptr)
+        || (set.insert(node->key).second
+            && hasNoDuplicateKeys(node->left, set)
+            && hasNoDuplicateKeys(node->right, std::forward<HashSet>(set)));
+}
+
+// Pre: node->height is correct.
+template <class Node>
+bool isBalancedAt(const Node* node) {
+    auto nodeHeight = [](const Node* node) -> int { return (node == nullptr) ? -1 : node->height; };
+    return (node == nullptr) || (std::abs(nodeHeight(node->left) - nodeHeight(node->right)) < 2);
+}
+
+template <class Node>
+bool isBST(const Node* node, const Node* parent = nullptr, bool isLeftChild = false) {
+    if (node == nullptr) {
+        return true;
+    }
+    if (parent == nullptr) {
+        return isBST(node->left, node, true) && isBST(node->right, node, false);
+    }
+    return ((isLeftChild && node->key < parent->key) || (!isLeftChild && node->key > parent->key))
+        && isBST(node->left, node, true) && isBST(node->right, node, false);
+}
+
+template <class Node>
+bool isAVL(const Node* node) {
+    return (node == nullptr)
+        || (isBST(node) && (realHeight(node) == node->height) && isBalancedAt(node)
+            && isAVL(node->left)
+            && isAVL(node->right));
+}
+
+template <class Tree>
+bool isValidAVL(const Tree& tree) {
+    const auto root = getRoot(tree);
+    return isAVL(root) && hasNoDuplicateKeys(root) && hasChildrenLinkedToParents(root)
+        && (realSize(root) == tree.size());
 }
 
 template <class Tree>
@@ -124,11 +187,11 @@ void insertRemoveStressTest(AVLTree<Key, Value>& tree, unsigned maxSize = 2000) 
 
 int main() {
     using std::cout;
-    srand(static_cast<unsigned>(time(nullptr))); // srand(time(0));
 
     AVLTree<int, char> tree;
 
     insertTest(tree);
+    assert(isValidAVL(tree));
 
     cout << "Size: " << tree.size() << '\n';
     cout << "Keys:\n";
@@ -145,9 +208,10 @@ int main() {
     cout << "New copied tree:\n";
     const auto copy = tree;
     print(copy);
-    assert(hasChildrenLinkedToParents(getRoot(copy)));
+    assert(isValidAVL(copy));
 
     removeTest(tree);
+    assert(isValidAVL(tree));
 
     cout << "Size: " << tree.size() << '\n';
     cout << "Keys:\n";
@@ -159,8 +223,11 @@ int main() {
         cout << value << '\n';
     }
 
-    insertRemoveStressTest(tree);
+    for (int i = 1; i < 3000; i = i * 2 + rand() % 2) {
+        insertRemoveStressTest(tree, i);
+    }
     // print(tree);
+    assert(isValidAVL(tree));
     cout << "Size: " << tree.size() << '\n';
 
     try {
